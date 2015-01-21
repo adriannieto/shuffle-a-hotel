5e7e3713e939000fdf32184fd18bb46eb53e4605
jercobyte <jercobyte@gmail.com>
2015-01-21 21:52:58 +0100

    [Frontend/Backend] Created new Sails app from scratch so that frontend and backend work together on the same server (CORS problems fixed).

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -9,13 +9,13 @@
  */
 module.exports = function(req, res, next) {
 
-    // User is allowed, proceed to controller
-    if (req.session.user) {
-      req.body.userId = req.session.user;
-      return next();
-    }
-    // User is not allowed
-    else {
-      return res.json({error : "Unauthorized"});
-    }
+  // User is allowed, proceed to the next policy, 
+  // or if this is the last policy, the controller
+  if (req.session.authenticated) {
+    return next();
+  }
+
+  // User is not allowed
+  // (default res.forbidden() behavior can be overridden in `config/403.js`)
+  return res.forbidden('You are not permitted to perform this action.');
 };
