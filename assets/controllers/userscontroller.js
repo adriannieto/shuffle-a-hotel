a9518b406164c1477e8d79ece0e0b1e74fa01f81
Juan María Frias Hidalgo <juanfriashidalgo@gmail.com>
2015-01-26 19:45:14 +0100

    [Frontend]: finished register for users

--- assets/controllers/userscontroller.js
+++ assets/controllers/userscontroller.js
@@ -1,12 +1,18 @@
 function userscontroller($scope,$http,$location){
 
 	$scope.createNewUser = function(){
-		$http.post('api/users',$scope.user)
-		.success(function(data, status, headers, config) {
-			alert("success");
-		}).error(function(data, status, headers, config) {
-			alert(data);
-		});
+		if($scope.user.password == $scope.user.checkpassword){
+			$http.post('api/users',$scope.user)
+			.success(function(data, status, headers, config) {
+				document.location.href = '/login.html';
+			}).error(function(data, status, headers, config) {
+				alert(data.error);
+			});	
+		}else{
+			alert("La contraseña no coincide");
+		}
+
+			
 	}
 }
 
