89102c5ebac191a7a495659f417efb6797ce4e7a
Adrián Nieto <adrian@lcc.uma.es>
2015-01-26 17:52:55 +0100

    [Backend] Enabled production in SailsJS
    Remember to disable session storage on MongoDB in config/session.js for development usage.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,33 +1,3 @@
-/**
- * Bootstrap
- * (sails.config.bootstrap)
- *
- * An asynchronous bootstrap function that runs before your Sails app gets lifted.
- * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
- *
- * For more information on bootstrapping your app, check out:
- * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
- */
-
 module.exports.bootstrap = function(cb) {
-
-
-  Hotel.create([{name:'Playa de la Luz', description: 'Awesome hotel', url:'awesomehotel.com', price: 120, location: {latitude: 36.723380, longitude: -4.414341}},
-				{name:'Malaga Palacio', description: 'Awesome hotel', url:'awesomehotel.com', price: 120, location: {latitude: 41.407381, longitude: 2.165970}},
-				{name:'NH Málaga', description: 'Awesome hotel', url:'awesomehotel.com', price: 120, location: {latitude: 43.274880, longitude: -7.985397}},
-				{name:'Hotel Palma Palmilla', description: 'Awesome hotel', url:'awesomehotel.com', price: 120},
-				{name:'Asperones Resort', description: 'Awesome hotel', url:'awesomehotel.com', price: 120},
-				{name:'4 de Diciembre Gran Lujo', description: 'Awesome hotel', url:'awesomehotel.com', price: 120},
-				{name:'AC Hoteles La Corta', description: 'Awesome hotel', url:'awesomehotel.com', price: 120},
-				{name:'El Banco de la Esquina', description: 'Awesome hotel', url:'awesomehotel.com', price: 120}]
-			).exec(function (err, hotel){
-				if(err)
-					console.log(err);
-				
-				console.log('Added a sample collection of hotels')
-  			});
-  
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
 };
